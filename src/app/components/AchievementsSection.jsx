1a93dd357e15fac6534f271d34139d58e160af36
sdhajwane <102373590+sdhajwane@users.noreply.github.com>
2023-11-01 17:33:23 +0530

    Update AchievementsSection.jsx

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -30,10 +30,10 @@ const achievements =[
 const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <div className="border-[#33353F] border rounded-md py-8 px-16 flex flex-row items-center justify-between">
+        <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
         {achievements.map((achievement, index)=>{
         return(
-            <div key={index} className="flex flex-col items-center justify-center mx-4">
+            <div key={index} className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0">
                 <h2 className="text-white text-4xl sm:text-xl font-bold ">
                 {achievement.value}
                 {achievement.postfix}
