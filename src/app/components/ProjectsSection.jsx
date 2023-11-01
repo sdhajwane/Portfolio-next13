8985c28f435db3c2e76942f6e8aa2ba48ddaaf6d
sdhajwane <102373590+sdhajwane@users.noreply.github.com>
2023-11-01 17:57:29 +0530

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -55,7 +55,7 @@ const ProjectsSection = () => {
   return (
     <section >
     <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12" >My Projects</h2>
-    <div className="text-white flex flex-row justify-center items-center gap-2  py-6">
+    <div className="text-white flex flex-row justify-center items-center gap-2 sm:w-50 py-6">
         <ProjectTag onClick={handleTagChange} name="All" isSelected={tag=== "All"} />
         <ProjectTag onClick={handleTagChange} name="NextJs" isSelected={tag=== "NextJs"} />
         <ProjectTag onClick={handleTagChange} name="Php" isSelected={tag=== "php"} />
